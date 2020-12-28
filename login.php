e56dfd6db050bdaf61847f46888113adbc592085
om4rkh4lid <52323807+om4rkh4lid@users.noreply.github.com>
2020-12-29 00:40:32 +0200

    Update login.php

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ login();
     <meta charset="utf-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="login.css" >
+    <link rel="stylesheet" href="css/login.css" >
 
 </head>
 
