511c69aaf42e4b4ddbae05c0c363f299dfb2cabb
Hassan Rady <hassan.khaled.rady@gmail.com>
2021-02-17 10:00:18 +0200

    after

--- includes/Admin/all_types/functions.php
+++ includes/Admin/all_types/functions.php
@@ -1,17 +1,7 @@
 <?php
 
-// getting connection
-include_once dirname(__FILE__, 3)."\\db_conn.php";
 
-// global variables
-include_once dirname(__FILE__, 3)."\\utils\\variables.php";
-
-// helper functions
-include_once dirname(__FILE__, 3)."\\utils\\helper.php";
-
-
-// form data retriever functions
-include_once dirname(__FILE__, 3)."\\utils\\form_functions.php";
+include_once dirname(__FILE__, 3) . "\\utils\\iniclude_utils_files.php";
 
 function deleteUser()
 {
