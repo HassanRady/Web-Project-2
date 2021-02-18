327a72deba6a821258c64e46ff5654917ac9d991
Hassan Rady <hassan.khaled.rady@gmail.com>
2021-02-18 15:22:25 +0200

    step 1 of connecting

--- includes/utils/variables.php
+++ includes/utils/variables.php
@@ -1,11 +1,13 @@
 <?php
 
+// Database tables
 $studentsTable = "students";
 $professorsTable = "professors";
 $tasTable = "tas";
 $sasTable = "sas";
 $adminsTable = "admins";
 
+// user's types 
 $studentsType = "student";
 $professorsType = "professor";
 $tasType = "ta";
@@ -13,6 +15,14 @@ $sasType = "sa";
 $adminsType = "admin";
 $userTypes = array($studentsType, $professorsType, $tasType, $sasType, $adminsType);
 
+// Records per list
 $rowsPerPage = 3;
 
+// Directory 
 $profileImageDir = "profile_images";
+
+// Paths
+$all_courses_path = "..\\admin\\available_courses_t.php";
+$my_profile_path = "../my_profile.php";
+$venues_path = "..\\admin\\venues.php";
+$students_list_paht = "..\\admin\\Students.php";
