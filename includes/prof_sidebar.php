506e37ed0445539462494275a141b28c0faac87d
AbdallahYasser1 <a.yasser9999@yahoo.com>
2021-03-14 22:54:25 +0200

    logout

--- includes/prof_sidebar.php
+++ includes/prof_sidebar.php
@@ -37,7 +37,8 @@ if(isset($_POST['logout-btn'])){
     <ul class="list-unstyled CTAs">
         <li>
             <form method="post">
-                <input type="submit" class="cta-logout" name="logout-btn" value="Logout">
+                <button type="submit" class="btn btn-block cta-logout" style="background-color: #fafafa; color: red;" name="logout-btn" value="Logout">Logout</button>
+
             </form>
         </li>
     </ul>
