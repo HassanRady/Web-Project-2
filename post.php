6add4e959054808ecfb23ab367658e8a74c68ecb
anthibo <47614266+anthibo@users.noreply.github.com>
2021-03-15 19:56:03 +0200

    updating ui for announcement/discussion screens

--- post.php
+++ post.php
@@ -16,68 +16,66 @@ if (isset($_GET['p_id'])) {
 ?>
 
 
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title>General Announcements</title>
+    <title>General Announcements</title>
 
-        <!-- Bootstrap CSS CDN -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-              integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
-              crossorigin="anonymous">
-        <!-- Our Custom CSS -->
-        <link rel="stylesheet" href="css/rootStyles.css">
-        <link rel="stylesheet" href="student/css/dispost.css">
-        <!-- Scrollbar Custom CSS -->
-        <link rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+          crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/rootStyles.css">
+    <link rel="stylesheet" href="student/css/dispost.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
-        <!-- Font Awesome JS -->
-        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-                integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-                crossorigin="anonymous"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-                integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-                crossorigin="anonymous"></script>
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
+            integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
+            crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
+            integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
+            crossorigin="anonymous"></script>
 
-    </head>
+</head>
 
 <body>
-
-
 
 
 <div class="wrapper">
     <!-- Sidebar  -->
     <?php
-        include_once "includes/utils/variables.php";
-        include_once dirname(__FILE__, 1) .DIRECTORY_SEPARATOR. "paths.php";
+    include_once "includes/utils/variables.php";
+    include_once dirname(__FILE__, 1) . DIRECTORY_SEPARATOR . "paths.php";
 
-        session_start();
-        $type = $_SESSION['type'];
+    session_start();
+    $type = $_SESSION['type'];
 
-        if ($type === $studentsType)
-            include $student_sidebar_path;
-        elseif ($type === $adminsType || $type == $sasType)
-            include $admin_sidebar_path;
-        else
-            include $professor_sidebar_path; ?>
+    if ($type === $studentsType)
+        include $student_sidebar_path;
+    elseif ($type === $adminsType || $type == $sasType)
+        include $admin_sidebar_path;
+    else
+        include $professor_sidebar_path; ?>
 
 
     <!-- Page Content  -->
     <div id="content">
 
-    <?php
-            if ($type === $studentsType)
-                include $student_navbar_path;
-            else
-                include $professor_navbar_path;
-            ?>
+        <?php
+        if ($type === $studentsType)
+            include $student_navbar_path;
+        else
+            include $professor_navbar_path;
+        ?>
 
         <div class="page-body">
             <!-- START HERE -->
@@ -92,19 +90,19 @@ if (isset($_GET['p_id'])) {
                 $post_id = $_POST['post_id'];
                 $votes = $_POST['votes'];
                 upVote($post_id, $the_user_id, $votes);
-                header('Location: post.php?p_id='.$post_id);
+                header('Location: post.php?p_id=' . $post_id);
             }
             if (isset($_POST['downvote'])) {
 
                 $post_id = $_POST['post_id'];
                 $votes = $_POST['votes'];
                 downVote($post_id, $the_user_id, $votes);
-                header('Location: post.php?p_id='.$post_id);
+                header('Location: post.php?p_id=' . $post_id);
             }
             if (isset($_POST['redo'])) {
                 $post_id = $_POST['post_id'];
                 redoVotePost($post_id, $the_user_id);
-                header('Location: post.php?p_id='.$post_id);
+                header('Location: post.php?p_id=' . $post_id);
             }
             $posts_result = getPost($the_post_id);
             while ($row = mysqli_fetch_assoc($posts_result)) {
@@ -121,21 +119,24 @@ if (isset($_GET['p_id'])) {
                         <p>
                             <?php echo $result_post_content; ?>
                         </p>
-
                         <div class="row">
                             <?php
                             // if user hadn't voted on the post yet, show him/her the upvote, downvote buttons
                             if (!checkIfVotedPost($the_post_id, $the_user_id)) {
                                 ?>
-                                <div class="col"><input type="submit" name="upvote" value="upvote"
-                                                        class="btn btn-primary"></div>
-                                <div class="col"><input type="submit" name="downvote" value="downvote"
-                                                        class="btn btn-danger"></div>
+                                <button type="submit" class="btn btn-outline-primary"
+                                        style=" background-color: rgb(31,108,236, 0.01); color: #000; border-color:rgb(31,108,236, 0.03) ; border-style: none;"
+                                        name="upvote">
+                                    <i class="fas fa-arrow-circle-up" style="color: rgb(31,108,236);"></i></button>
+                                <span style=" margin-right: 15px; margin-left: 10px; padding-top: 6px;"><p>Votes: <?php echo $result_post_votes; ?> </p></span>
+                                <button type="submit" class="btn btn-outline-primary"
+                                        style="background-color: rgb(31,108,236, 0.01); color: #000; border-color:rgb(31,108,236, 0.03) ; border-style: none;"
+                                        name="downvote">
+                                    <i class="fas fa-arrow-circle-down" style="color: rgb(31,108,236);"></i></button>
                             <?php } else {
                                 echo "<div class='col'><input type='submit' name='redo' value='redo' class='btn btn-primary'></div>";
                             }
                             ?>
-                            <div class="col"><p>Votes: <?php echo $result_post_votes; ?> </p></div>
                             <input type="hidden" name="post_id" value="<?php print $the_post_id; ?>"/>
                             <input type="hidden" name="votes" value="<?php print $result_post_votes; ?>"/>
                         </div>
@@ -173,7 +174,7 @@ if (isset($_GET['p_id'])) {
                 if (isset($_POST['delete_comment'])) {
                     $comment_id = $_POST['delete_comment_id'];
                     deleteComment($comment_id);
-                    header('Location: post.php?p_id='.$post_id);
+                    header('Location: post.php?p_id=' . $post_id);
                 }
                 //getting all the comments
                 $comments_results = getAllComments($the_post_id);
@@ -190,11 +191,12 @@ if (isset($_GET['p_id'])) {
                         <p><?php echo $content; ?></p>
                         <?php
                         //checking if the current user has posted a comment so if he did he can delete it
-                        if ($id_user == $the_user_id ) {
+                        if ($id_user == $the_user_id) {
                             ?>
                             <form action="post.php?p_id=<?php echo $the_post_id; ?>"
                                   method="post" role="form">
-                                <input type="submit"  value="Delete Comment" name="delete_comment" class="btn btn-primary">
+                                <input type="submit" value="Delete Comment" name="delete_comment"
+                                       class="btn btn-primary">
                                 <input type="hidden" name="delete_comment_id" value="<?php print $comment_id; ?>"/>
                             </form>
                             <?php
@@ -210,7 +212,7 @@ if (isset($_GET['p_id'])) {
 
                 <div class="add-comment">
                     <h4>Leave a Comment:</h4>
-                                                    <!--  redirecting to the current page                   -->
+                    <!--  redirecting to the current page                   -->
                     <form action="post.php?p_id=<?php echo $the_post_id; ?>"
                           method="post"
                           role="form">
