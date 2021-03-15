49744eba413fa33f2b0a5a6211af673da187d2c1
mariiam-aly <76651233+mariiam-aly@users.noreply.github.com>
2021-03-15 16:08:54 +0200

    Update editprofile.php

--- editprofile.php
+++ editprofile.php
@@ -68,7 +68,7 @@ updateProfile();
           <div class="col-md-12 order-md-1 col-lg-12">
             <h4 class="mb-3">Edit profile</h4>
             <hr class="mb-4">
-            <form novalidate action="/" method="POST" onsubmit="return !!(empty_field() & validate_names() & validate_GurdianNumber() & validate_MobileNumber() &  validate_HomeNumber() & validate_1stpassword() & validate_2ndpassword())">
+            <form novalidate action="/" method="POST" onsubmit="return !!(empty_field1() & validate_names() & validate_GurdianNumber() & validate_MobileNumber() &  validate_HomeNumber() & validate_1stpassword() & validate_2ndpassword())">
               <div class="row">
                 <div class="col-lg-4 col-md-12 mb-3">
                   <label for="firstName">First name</label>
